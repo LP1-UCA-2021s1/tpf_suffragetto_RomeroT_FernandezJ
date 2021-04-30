e7cf83c4fcfe8d63a05bc7b84864492e2df23421
JulioFL157 <july.j.fer@gmail.com>
2021-04-29 21:36:41 -0400

    prototipo_menu

--- src/Game_suffragetto_tp.c
+++ src/Game_suffragetto_tp.c
@@ -13,6 +13,56 @@
 #include "Game_suffragetto_tp.h"
 
 void menu(char tab[TAM_TAB][TAM_TAB]){
+	int opcion=0;
+		do
+		{
+			//system("cls");
+			printf("Bienvenido al juego  SUFFRAGETTO \n");
+			printf("MENU PRINCIPAL\n");
+			printf("1) JUGAR \n");
+			printf("2) REGLAS DE JUEGO \n");
+			printf("3) INSTRUCCIONES DE USO \n");
+			printf("4) CREDITOS \n");
+			printf("5) SALIR \n\n");
+			printf("Ingrese el numero de la opcion que desea ejecutar:");
+			scanf("%d",&opcion);
+			fflush(stdin);//limpia el buffer de entrada cuando se ingresa un valor no válido
+			switch(opcion)
+			{
+				case 1:
+					//system("cls");
+
+					//system("PAUSE");
+					break;
+				case 2:
+					//system("cls");
+					printf("          ----------REGLAS DE JUEGO----------\n\n");
+					printf(" -Piezas: 21 fichas por jugador; 5 fichas violetas y 16 fichas verdes para un jugador (suffragetto), 5 fichas blancas y 16 fichas azules para el otro jugador (policía). Las fichas violetas representan a lo..\n\n -El objetivo de ambos jugadores es ocupar el área protegida por sus enemigos. Así, los policías desean ocupar el “Albert Hall” mientras que los suffragetto desean ocupar la región “House of Commons”. De esta forma el juego finaliza cuando una de estas dos regiones es ocupada completamente, y gana el jugador que ubicó 6.\n\n -.\n\n -.\n\n -.\n\n ");
+					//system("PAUSE");
+					break;
+				case 3:
+					//system("cls");
+					printf("          ----------INSTRUCCIONES PARA USO DEL PROGRAMA---------\n\n");
+					printf(" -El programa decidira al azar a quien corresponde el primer turno.\n\n -Para mover las piezas se deben introducir las coordenadas de la pieza a mover y las de la posicion a mover.\n\n -Las coordendas del tablero van del 1 al 17 para las filas y columnas.\n\n -El programa se encargara de advertirle de errores o movimientos invalidos.\n\n -Tambien se encargara de ............. si se da el caso.\n\n -Luego de cada jugada usted vera cuantas........... quedan fuera del tablero y cuantas fueron trapadas por ...........\n\n ");
+					//system("PAUSE");
+					break;
+				case 4:
+					//system("cls");
+					printf("           -----------CREDITOS----------\n\n");
+					printf("          ----------PROGRAMADORES----------\n\n");
+					printf("                    Julio Fernandez\n\n                          e\n\n                      Tania Romero\n\n");
+					//system("PAUSE");
+					break;
+				case 5:
+					break;
+				default:
+					//system("cls");
+					printf("Ha ingresado un valor invalido\n");
+					printf("Favor ingrese una opcion del MENU PRINCIPAL\n");
+					//system("PAUSE");
+					break;
+			}
+		}while(opcion!=5);
 
 }
 
